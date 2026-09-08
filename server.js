fad8bd85d56eddc3738ebce3f048efff278c23c4
Miner Ertizani <erti1kill@gmail.com>
2026-09-08 18:48:54 +0200

    Homework is created

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+const HOST = "localhost";
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+    res.status(418).json({time: new Date().toLocaleTimeString()});
+});
+
+app.listen(PORT, HOST, () => {
+    console.log("Server is running at http://localhost:8000");
+});
